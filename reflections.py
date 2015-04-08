d0e03f931da2558fd8b193af872b2b507f22091c
taydakov <lev.taydakov@gmail.com>
2015-04-07 20:00:32 -0700

    Back forth looping

--- reflections.py
+++ reflections.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
-print "Hello, world"
+import time
+
+def action( param ):
+  print param
+
+curr = 1
+dir  = +1
+while True:
+  action(curr)
+  curr += dir
+  if curr >= 4:
+  	dir = -1
+  if curr <= 1:
+  	dir = +1
+  time.sleep(0.2)
