e4d00c6e91729ba11d445105836bf2edbde56605
taydakov <lev.taydakov@gmail.com>
2015-04-07 20:11:24 -0700

    Implement all the logic

--- reflections.py
+++ reflections.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python
 
 import time
+import RPi.GPIO as GPIO
 
-def action( param ):
-  print param
+# Ordered Raspberry Pi pins
+pins = [ 4, 17, 27, 22 ];
 
-curr = 1
+# Fire pin (high voltage level) function
+def firepin( param ):
+  # Cool all the pins (low voltage level)
+  for pin in pins:
+    GPIO.output(pin, GPIO.LOW)
+  currpin = pins[ param ]
+  GPIO.output(currpin, GPIO.HIGH)
+
+# Preparing Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+for pin in pins:
+  GPIO.setup (pin, GPIO.OUT)
+  GPIO.output(pin, GPIO.LOW)
+
+# Forth-back loop
+curr = 0
 dir  = +1
 while True:
-  action(curr)
+  firepin( curr )
   curr += dir
-  if curr >= 4:
+  if curr >= 3:
   	dir = -1
-  if curr <= 1:
+  if curr <= 0:
   	dir = +1
-  time.sleep(0.2)
+  time.sleep( 0.2 )
